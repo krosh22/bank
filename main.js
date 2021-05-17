d8fefac7a2d25061cae2983fcd87e051665f641d
krosh22 <Olga2018r@yandex.ru>
2021-05-17 17:03:29 +0300

    fixs

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 const myModal = new HystModal({
     linkAttributeName: "data-hystmodal",
 });
+
+$(".slider-class").slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    responsive: [
+        {
+            breakpoint: 2000,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 1024,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 768,
+            settings: "unslick"
+        },
+        {
+            breakpoint: 760,
+            settings: {
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        },]
+});
